7d51d0fbff36118de87dfbc5052fa31926990c7b
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-02-02 20:23:08 +0100

    Add project base

---
+++ src/main/java/net/relics_rpgs/Relics.java
@@ -0,0 +1,11 @@
+package net.relics_rpgs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Relics implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/net/relics_rpgs/client/RelicsClient.java
@@ -0,0 +1,10 @@
+package net.relics_rpgs.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RelicsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
